da7e8c02b4dce7808e63059874f27129b0695045
Devil5614737 <kaushiksheel9@gmail.com>
2022-12-13 23:11:21 +0530

    added loading skeletons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,55 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import { Container,Grid,Button } from '@mui/material'
-import { Header } from '../Components/Header'
-import { Blog } from '../Components/Blog'
-import {useNavigate} from 'react-router-dom';
-import {fetchBlogs} from '../api/request'
-import { IBlog } from '../Interfaces/IBlog';
-
+import { Container, Grid, Button } from "@mui/material";
+import { Header } from "../Components/Header";
+import { Blog } from "../Components/Blog";
+import { useNavigate } from "react-router-dom";
+import { fetchBlogs } from "../api/request";
+import { IBlog } from "../Interfaces/IBlog";
+import { SkeletonComp } from "../Components/Skeleton";
 
 export default function Home() {
-  const[blogs,setBlogs]=useState<IBlog[]>([])
-  const navigate=useNavigate();
+  const [blogs, setBlogs] = useState<IBlog[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
 
+  const getBlogs = async () => {
+    setIsLoading(true);
+    const { data: blogs } = await fetchBlogs();
+    blogs && setIsLoading(false);
+    setBlogs(blogs);
+  };
 
-const getBlogs=async()=>{
-  const {data:blogs}=await fetchBlogs();
-  setBlogs(blogs)
-}
-
-useEffect(()=>{
-  getBlogs()
-},[]);
-
-
-
+  useEffect(() => {
+    getBlogs();
+  }, []);
 
   return (
-  <>
-  <Container maxWidth='md'>
-    <Header title='Blog Articles'/>
-    <Button 
-    onClick={()=>navigate('/new-blog')}
-sx={{marginTop:2}}
-size='medium'
-variant="outlined">New Article</Button>
-  <main style={{marginTop:23}}>
-    
-    <Grid container spacing={2}>
-      {blogs?.map((blog:IBlog)=>
-  <Grid key={blog._id} item xs={12} >
-  <Blog blog={blog}/>
-  </Grid>
-      )}
-  
-</Grid>
-  </main>
-  </Container>
-  </>
-  )
+    <>
+      <Container maxWidth="md">
+        <Header title="Blog Articles" />
+        <Button
+          onClick={() => navigate("/new-blog")}
+          sx={{ marginTop: 2 }}
+          size="medium"
+          variant="outlined"
+        >
+          New Article
+        </Button>
+        <main style={{ marginTop: 23 }}>
+          {isLoading ? (
+            <SkeletonComp />
+          ) : (
+            <Grid container spacing={2}>
+              {blogs?.map((blog: IBlog) => (
+                <Grid key={blog._id} item xs={12}>
+                  <Blog blog={blog} />
+                </Grid>
+              ))}
+            </Grid>
+          )}
+        </main>
+      </Container>
+    </>
+  );
 }
